26bd0309bb2f97c46f4c172a2d792edaa19a33d0
NikolaiKononov <nikolai.kononov@cytiva.com>
2021-08-31 15:26:12 +0300

    index update no conflicts

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ console.log("1.4.0-bugfoxes");
 console.log("1.4.0-bugfoxes");
 console.log("1.4.0-bugfoxes");
 console.log("2.0.2-new-release-flow");
+console.log("2.1.0-update-only-package-json-no-conflicts");
