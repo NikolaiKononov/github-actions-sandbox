24daa68485c46d1cf77de4653235e4aa6ec37ad7
NikolaiKononov <nikolai.kononov@cytiva.com>
2021-08-31 14:15:26 +0300

    conflict added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 console.log("1.4.0-bugfoxes");
 console.log("1.4.0-bugfoxes");
 console.log("1.4.0-bugfoxes");
-console.log("1.4.0-bugfoxes");
+console.log('conflict check');
 console.log("1.4.0-bugfoxes");
 console.log("1.4.0-bugfoxes");
